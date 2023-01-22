aa001781cadd0830b552cbd18953e07682a1cafd
pandekar <pandeadhistanaya@gmail.com>
2023-01-22 13:57:31 +0800

    setup IBOutlet and IBAction

---
+++ Tipsy/Controller/ResultsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Pande Adhistanaya on 22/01/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
